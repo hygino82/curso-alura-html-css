bcac0ca368a0aeedac994e23e46a7aee395a6b65
Adroaldo <hygino@msn.com>
2026-07-14 13:15:48 -0300

    adicionado evento de curtida em vários botões

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-let curtidas = 0;
+const botoes = document.querySelectorAll("button");
 
-const botaoCurtir = () => {
-  curtidas++;
-  console.log(`Curtidas: ${curtidas}`);
-  document.getElementById("resultado-curtidas").innerHTML =
-    `<p>${curtidas} curtidas</p>`;
-};
+botoes.forEach(function(botao) {
+    botao.addEventListener("click", botaoClicado);
+
+    function botaoClicado() {
+        console.log("fui clicado");
+        let texto = botao.querySelector("span");
+        texto.textContent++;
+    }
+});
